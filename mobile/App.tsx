3f1e6e46262271b21d71e257442abfcda3c229e5
Lucas Andrade <lucasantos.dev@gmail.com>
2022-11-04 21:13:58 -0300

    create authentication with google

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,13 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
+import { THEME } from './src/styles/theme'
+
 import { SignIn } from './src/screens/SignIn'
 import { Loading } from './src/components/Loading'
 
-import { THEME } from './src/styles/theme'
+import { AuthContextProvider } from './src/contexts/AuthContext'
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,13 +18,15 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
 
-      { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <SignIn /> : <Loading /> }
+      </AuthContextProvider>
     </NativeBaseProvider>
   )
 }
