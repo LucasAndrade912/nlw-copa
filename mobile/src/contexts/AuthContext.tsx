fc43f7a79246268b413167608c20b533b19e8364
Lucas Andrade <lucasantos.dev@gmail.com>
2022-11-06 18:44:04 -0300

    authenticate user

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -3,6 +3,8 @@ import * as GoogleProvider from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
 
+import { api } from '../lib/api'
+
 WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
@@ -45,7 +47,20 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   }
 
   async function signInWithGoogle(accessToken: string) {
-    console.log('TOKEN DE AUTENTICAÇÃO ===>', accessToken)
+    try {
+      setIsUserLoading(true)
+
+      const tokenResponse = await api.post('/users', { accessToken })
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`
+
+      const userInfoResponse = await api.get('/me')
+      setUser(userInfoResponse.data.user)
+    } catch (err) {
+      console.log(err)
+      throw err
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(() => {
