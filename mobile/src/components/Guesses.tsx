d50d498b57d1d0a169278b5436bbfa342e159e61
Lucas Andrade <lucasantos.dev@gmail.com>
2022-11-08 22:50:12 -0300

    add feature list games

--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -1,14 +1,65 @@
-import { Box } from 'native-base'
+import { useState, useEffect } from 'react'
+import { FlatList, useToast } from 'native-base'
+
+import { api } from '../lib/api'
+
+import { Loading } from './Loading'
+import { Game, GameProps } from './Game'
+import { EmptyMyPoolList } from './EmptyMyPoolList'
 
 interface Props {
   poolId: string
+  code: string
 }
 
-export function Guesses({ poolId }: Props) {
+export function Guesses({ poolId, code }: Props) {
+  const [isLoading, setIsLoading] = useState(false)
+  const [games, setGames] = useState<GameProps[]>([])
+  const [firstTeamPoints, setFirstTeamPoints] = useState('')
+  const [secondTeamPoints, setSecondTeamPoints] = useState('')
+
+  const toast = useToast()
+
+  async function fetchGames() {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(`/pools/${poolId}/games`)
+      setGames(response.data.games)
+    } catch (err) {
+      console.log(err)
+      toast.show({
+        title: 'Não foi possível carregar os jogos',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchGames()
+  }, [poolId])
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
-    <Box>
-
-    </Box>
+    <FlatList
+      data={games}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          onGuessConfirm={() => {}}
+        />
+      )}
+      _contentContainerStyle={{ pb: 10 }}
+      ListEmptyComponent={() => <EmptyMyPoolList code={code} />}
+    />
   )
 }
